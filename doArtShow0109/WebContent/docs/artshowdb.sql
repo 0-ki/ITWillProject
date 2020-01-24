cefc130f5aca58638653370a5e59d679147f229f
YGKIM <rladudrl286@naver.com>
2020-01-24 15:51:45 +0900

    docs에 SNS로그인 내용 추가

--- doArtShow0109/WebContent/docs/artshowdb.sql
+++ doArtShow0109/WebContent/docs/artshowdb.sql
@@ -26,7 +26,10 @@ email   varchar(45)  not null primary key, -- 이메일
 name    varchar(10)  not null, -- 이름
 birth    varchar(45)  not null, -- 생년월일
 gender   varchar(10)  not null, -- 성별
-pw      varchar(45) not null -- 비밀번호
+pw      varchar(45)  null, -- 비밀번호
+profileImg varchar(45) null,
+kakaoId varchar(30) null,
+naverId varchar(30) null
 ) default character set utf8 collate utf8_general_ci;
 
 insert into artshowdb.member(email, name, birth, gender, pw) values('jungmi0008@naver.com','길정미','1993-08-16','여성','Qp2ql134!');
